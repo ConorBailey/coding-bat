8e4c10e00fc1c1d58b900f349e3c2d4c40862ba8
Conor Bailey <conorbaileybus@gmail.com>
2023-07-26 20:41:46 +0200

    added firstlast6

--- firstLast6/src/index.ts
+++ firstLast6/src/index.ts
@@ -1,14 +1,14 @@
 /* 
-Given an array of ints,
-return true if 6 appears as either the first or last element in the array.
-The array will be length 1 or more.
+
+Given a string, return a new string where "not " has been added to the front.
+However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
 
 
-firstLast6([1, 2, 6]) → true
-firstLast6([6, 1, 2, 3]) → true
-firstLast6([13, 6, 1, 2, 3]) → false
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
 */
 
-export function firstLast6(numbers: number[]) {
-  return numbers[0] == 6 || numbers[numbers.length - 1] == 6;
+export function notString(notString: string): string {
+
 }
--- firstLast6/test/index.test.ts
+++ firstLast6/test/index.test.ts
@@ -1,32 +1,32 @@
 import { assert } from "chai";
-import { firstLast6 } from "../src/index";
+import { notString } from "../src/index";
 
 /* 
 
-Given an array of ints,
-return true if 6 appears as either the first or last element in the array.
-The array will be length 1 or more.
+
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
 
 
-firstLast6([1, 2, 6]) → true
-firstLast6([6, 1, 2, 3]) → true
-firstLast6([13, 6, 1, 2, 3]) → false
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
 
 */
 
-describe("firstLast6", () => {
-  it("should return true if 6 appears in the array in the first or last position", () => {
-    const result = firstLast6([1, 2, 6]);
-    assert.equal(result, true);
-  });
 
-  it("should return true if 6 appears in the array in the first or last position", () => {
-    const result = firstLast6([6, 1, 2, 3]);
-    assert.equal(result, true);
-  });
+describe ("notString", () => {
+  it("should return not candy as there is no not infront of the word candy", () => {
+  const result = notString("candy")
+  assert.equal(result, "not candy")
+  })
+  
+  it("should return not x as there is no not infront of the x", () => {
+  const result = notString("x")
+  assert.equal(result, "not x")
+  })
 
-  it("should return false if 6 doesnt appear in first or last position of the array", () => {
-    const result = firstLast6([12, 6, 1, 2, 3]);
-    assert.equal(result, false);
-  });
-});
+  it("should return not bad as there is already a not infront ", () => {
+    const equal = notString("not bad")
+    assert.equal(result, "not bad")
+  })
+})
